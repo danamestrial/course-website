e0ba42ee431df866b1c66a4dfde167df1c159d5d
danamestrial <chayapolbun@gmail.com>
2021-08-19 18:22:38 +0700

    Version 1

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://beca0a61b96da0:2ee13097@us-cdbr-east-04.cleardb.com/heroku_130f9e0233f5e49'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ ==  '__main__':
+    app.run(debug=True)
